9e7b98fe1bc7b906c231cf67282a9586016ed115
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-07 04:13:34 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs , not browser 
+
+// tc39 and mdn for specifications -> remember it ..
+
+let name = "nitesh" // string dataype 
+let age =19 // number 
+let isloggedin = false // bollean 
+
+// null -> it is a standlone value , representation of an empty value
+// undefined = value is currently not assigned 
+// symbol = to find uniqueness 
+
+console.log(typeof "nitesh")
+console.log(typeof 19)
+console.log(typeof true)
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined -> it is itself a type 
