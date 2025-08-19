3c791080ad055f0f8cc19fc35942160c338c48c1
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-19 16:39:11 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitive -> call by value 
+// 7 types -> string , number , boolean , null , undefined , symbol , BigInt 
+
+// reference type -> non primitive 
+// array , objects , functions 
+
+// js is dynamically typed language  -> const num = 100 -> js itself consider num as a number 
+
+// null means empty not zero 
+
+// use of symbol 
+// const id = Symbol('133')
+// const anotherid = Symbol('133') 
+// console.log(id === anotherid); // return false  
+
+
+
+
+
