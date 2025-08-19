16cde03fcc03756ad610a710739facb6a58a1283
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-19 16:04:22 +0000

    conversion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// always try to compare two similiar data types 
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >=0); 
+// the equality check and comparision works diffrently comparision convert null to a number treating it as 0 thats why 3 ->true 
+// and 1 -> false 
+// === also check datatype is equal or not 
+console.log("2"==="2"); // return true 
+console.log("2"===2); // return false 
+
+
+
+
+
+
+
