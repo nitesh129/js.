dcc187b4d9996313d3e4878cb3c2ac0b85511e3e
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-19 15:48:52 +0000

    conversions

---
+++ 01_basics/03_conversionope.js
@@ -0,0 +1,30 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+// convatenation of string 
+let str1="nitesh"
+let str2=" dayma"
+let str3=str1+str2
+console.log(str3);
+// why string to number convsrsion is difficult 
+// console.log("1" + 2); // gives 12 
+// console.log("1" +"2"); // gives 12 
+// console.log("1" + 2 + 2); // gives 122 why ??  if string comes first everything treated as string 
+// console.log(1 + 2 + "3"); // gives 33 why ??  if string at end then operation will be done 
+
+// TRICKY CONVERSIONS 
+// console.log(+true);
+// console.log(+""); 
+
+// let num1 = 23 
+// let str=String(num1)
+// console.log(str);
+// console.log(typeof str);
+ 
+// 
+
+
+
+
+
+
