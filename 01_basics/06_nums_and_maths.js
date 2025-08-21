aa12b372fc2c41990e7043ec3f0bf308d1c3d53b
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-21 19:01:36 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+// const score =400
+// const balance = new Number(100)
+// console.log(balance);  
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // 100.00
+
+// const otherNumber = 1164.696
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds= 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++ maths ++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.6));
+console.log(Math.sqrt(36)) 
+
+// most important 
+console.log(Math.random()); // value always lie btw 0 and 1 
+console.log((Math.random()*10) + 1);  // to avoid case of 0 
+
+const min = 10 
+const max = 20 
+console.log(Math.floor(Math.random()*(max-min + 1)) + min); // math.floor use to get exact value (only onr value) 
+
+
+
+
+
