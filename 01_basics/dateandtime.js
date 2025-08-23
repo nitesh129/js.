fdd4c13260a467d0ac3c6752279a1bac4fb4b14f
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-08-23 03:54:01 +0000

    Data and Time

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,37 @@
+// Dates 
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString); 
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate); // object return 
+// let mynewDate = new Date(2025,8,22) //-> return  sep month because months starts from 0
+// console.log(mynewDate.toDateString());
+
+// // to check the result of quiz as a time stamp 
+// let mytimestamp =  Date.now()
+
+// console.log(mytimestamp)
+// console.log(mynewDate.getTime());
+// // now compare both 
+
+
+// let newdate = new Date(22-2-2025)
+// console.log(newdate);
+// console.log(newdate.getMonth()); // as same as get day and get year etc
+
+
+
+
+
+
+
+
+
+
+
+
+
